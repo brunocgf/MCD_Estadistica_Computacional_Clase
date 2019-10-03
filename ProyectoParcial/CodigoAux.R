3faac994193ffd8b41d46796012096013a8459a5
Bruno Gonzalez <brunocgf@gmail.com>
2019-10-03 13:01:57 -0500

    Actualizacion 1 proyecto parcial

--- ProyectoParcial/CodigoAux.R
+++ ProyectoParcial/CodigoAux.R
@@ -3,17 +3,6 @@ library(tidyverse)
 
 data(tea)
 
-tea <- tea %>% 
-  as_tibble %>% 
-  select(how, price, sugar)
-
-tipo <- tipo %>% select(how, prop_how = `%`)
-tabla_2 <- tea %>%
-  count(how, price) %>% 
-  group_by(how) %>% 
-  mutate(prop = round(100 * n / sum(n))) %>% 
-  select(-n) 
-tabla_2 %>% spread(how, prop, fill = 0) %>% formatear_tabla()
 
 tabla <- tea %>% 
   count(how, price) %>% 
@@ -23,31 +12,56 @@ tabla <- tea %>%
   mutate(prom_prop = mean(prop_price)) %>% 
   mutate(perfil = (prop_price / prom_prop - 1) %>% round(2))  
 
-precio_prom <- tabla %>% 
-  distinct(price, prom_prop) %>%
-  mutate(promedio = round(prom_prop)) %>% 
-  select(price, promedio)
+
 tabla_perfil <- tabla %>%   
   select(how, price, perfil) %>% 
   spread(how, perfil, fill = -1) 
-tabla_2 <- tabla_perfil %>% 
-  gather(how, prop_price, -price)
-if_profile <- function(x){
-  any(x < 0) & any(x > 0)
+
+
+#Segunda forma (solo como ejercicio, no examen)
+
+tabla2 <- tea %>% 
+  count(how, price) %>% 
+  group_by(how) %>% 
+  mutate(prop_price = (100 * n / sum(n))) %>% 
+  ungroup() %>% 
+  mutate(prom_prop = mean(prop_price)) %>% 
+  mutate(perfil = (prop_price / prom_prop - 1) %>% round(2)) 
+
+tabla_perfil2 <- tabla2 %>%   
+  select(how, price, perfil) %>% 
+  spread(how, perfil, fill = -1) 
+
+
+# 1. Utiliza bootstrap para crear intervalos de confianza sobre los perfiles de la última tabla.
+
+# función bootstrap
+perfiles_boot <- function(x){
+  m <- sample_n(x, size =  300 , replace = TRUE)
+  tabla <- m %>% 
+    count(how, price) %>% 
+    group_by(how) %>% 
+    mutate(prop_price = (100 * n / sum(n))) %>% 
+    group_by(price) %>% 
+    mutate(prom_prop = mean(prop_price)) %>% 
+    mutate(perfil = (prop_price / prom_prop - 1) %>% round(2))
+  tabla
 }
-marcar_tabla_fun <- function(corte, color_1 = "darkgreen", color_2 = "red"){
-  fun_marcar <- function(x){
-    kableExtra::cell_spec(x, "html", 
-                          color = ifelse(x <= -corte, color_1, 
-                                         ifelse(x >= corte, color_2, "lightgray")))   
-  }
-  fun_marcar
-}
-marcar <- marcar_tabla_fun(0.25, "red", "black")
-tab_out <- tabla_perfil %>% left_join(precio_prom) %>%
-  arrange(desc(`tea bag`)) %>% 
-  #mutate_if(if_profile, marcar) %>% 
-  knitr::kable(format = "html", escape = F, digits = 2) %>% 
-  kableExtra::kable_styling(bootstrap_options = c( "hover", "condensed"), 
-                            full_width = FALSE)
-tab_out
+
+# Repeticiones
+
+perfiles_rep <- rerun(1000, perfiles_boot(tea)) %>% map_dfr(~.x)
+
+
+# Error estandard
+
+perfiles_se <- perfiles_rep %>% 
+  group_by(how, price) %>% 
+  summarise(se = sd(perfil))
+
+
+# Intervalos
+
+perfiles_int <- tabla %>% 
+  left_join(perfiles_se) %>% 
+  mutate(Int_inf = perfil+qnorm(0.025)*se, Int_sup = perfil+qnorm(0.975)*se)
